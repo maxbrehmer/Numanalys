2913a69039d146a53218b9fbef6bfd934cd94ce3
maxbrehmer <max.brehmer01@gmail.com>
2022-05-23 00:54:13 +0200

    lab 6 updated

--- lab6.py
+++ lab6.py
@@ -57,15 +57,15 @@ def jacobi_list(M, N, leftbc, rightbc):
     y.append(rightbc)
     return x, y
 
-def cg(A, b, x_cg, maxit):
-    rk = np.dot(A,x_cg) - b
-    p = -rk
+def cg(A,b,x_cg,maxit):
+    rk = b-np.dot(A,x_cg)
+    p = rk
     for k in np.arange(maxit):
-        alpha = -np.dot(np.transpose(rk), p) / np.dot(np.transpose(p), np.dot(A, p))
+        alpha = np.dot(np.transpose(rk), rk)/np.dot(np.transpose(p), np.dot(A, p))
         x_cg = x_cg + alpha*p
-        rkp1 = np.dot(A,x_cg) - b
-        beta = np.dot(np.transpose(rkp1), np.dot(A, p)) / np.dot(np.transpose(p), np.dot(A, p))
-        p = -rkp1 + beta*p
+        rkp1 = rk - alpha * np.dot(A, p)
+        beta = np.dot(np.transpose(rkp1), rkp1)/np.dot(np.transpose(rk), rk)
+        p = rkp1 + beta * p
         rk = rkp1
     return x_cg
 
@@ -78,6 +78,7 @@ def cg_list(M, N, leftbc, rightbc):
     return x, y
 
 ''' PART 1 '''
+# Question 3
 N = 5
 iter = 100
 leftbc = 1
@@ -93,13 +94,64 @@ x, y = ComputeAnalyticalSolution(N, leftbc, rightbc)
 
 plt.plot(x, y, label="analytical")
 plt.plot(x_j, y_j, label="jacobi method")
+plt.title("N=5, 100 iterations")
 plt.legend()
-plt.show()
+#plt.show()
 plt.clf()
+
+print(str(np.linalg.cond(A)) + '  Condition number of A when N=5 | ' + str(iter) + ' iterations')
+
+# Question 4
+N = 5
+iter = 35
+leftbc = 1
+rightbc = 2*np.sin(np.pi/2)
+A = subroutine_a(N)
+F = subroutine_b(N, leftbc, rightbc)
+x_0 = np.ones((N, 1))
+
+x_k = jacobi(A, F, iter, x_0)
+x_j, y_j = jacobi_list(x_k, N, leftbc, rightbc)
+
+x, y = ComputeAnalyticalSolution(N, leftbc, rightbc)
+
+plt.plot(x, y, label="analytical")
+plt.plot(x_j, y_j, label="jacobi method")
+plt.title("N=5, 35 iterations")
+plt.legend()
+#plt.show()
+plt.clf()
+
+# Question 5
+N = 10
+iter = 200
+leftbc = 1
+rightbc = 2*np.sin(np.pi/2)
+A = subroutine_a(N)
+F = subroutine_b(N, leftbc, rightbc)
+x_0 = np.ones((N, 1))
+
+x_k = jacobi(A, F, iter, x_0)
+x_j, y_j = jacobi_list(x_k, N, leftbc, rightbc)
+
+x, y = ComputeAnalyticalSolution(N, leftbc, rightbc)
+
+plt.plot(x, y, label="analytical")
+plt.plot(x_j, y_j, label="jacobi method")
+plt.title("N=10, 200 Iterations")
+plt.legend()
+#plt.show()
+plt.clf()
+
+print(str(np.linalg.cond(A)) + '  Condition number of A when N=10 | ' + str(iter) + ' iterations')
+
+print(str(np.linalg.cond(subroutine_a(20))) + '  Condition number of A when N=20 | ' + str(iter) + ' iterations')
+print(str(np.linalg.cond(subroutine_a(40))) + '  Condition number of A when N=40 | ' + str(iter) + ' iterations')
 
 ''' PART 2 '''
 N = 100
 iter = 100
+k = 15000
 leftbc = 1
 rightbc = 2*np.sin(np.pi/2)
 A = subroutine_a(N)
@@ -109,10 +161,14 @@ x_0 = np.ones((N, 1))
 A_spd = -1*A
 F_spd = -1*F
 
-x_cg_k = cg(A_spd, F_spd, x_0, iter)
-x_cg, y_cg = cg_list(x_cg_k, N, leftbc, rightbc)
+#x_cg_k = cg(A_spd, F_spd, x_0, iter)
+#x_cg, y_cg = cg_list(x_cg_k, N, leftbc, rightbc)
 
-plt.plot(x, y, label="analytical")
-plt.plot(x_cg, y_cg, label="conjugate-gradient")
-plt.legend()
-plt.show()
+#x_k = jacobi(A, F, k, x_0)
+#x_j, y_j = jacobi_list(x_k, N, leftbc, rightbc)
+
+#plt.plot(x, y, label="analytical")
+#plt.plot(x_cg, y_cg, label="conjugate-gradient (k = 100)")
+#plt.plot(x_j, y_j, label="jacobi method (k = 15 000)")
+#plt.legend()
+#plt.show()
